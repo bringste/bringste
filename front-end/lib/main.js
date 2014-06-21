28b2019e5b4d51787369c734965ed1c1fe83b5e6
TomKnig <hi@tomknig.de>
2014-06-21 19:27:09 +0200

    added newlist page and therefor auto generateable inputfields

--- front-end/lib/main.js
+++ front-end/lib/main.js
@@ -7,3 +7,18 @@ $('.card-wrapper > .card-small > .table-view').click(function () {
 $('.checkbox').click(function () {
   $(this).toggleClass('checkbox-checked');
 });
+
+var cloneTextField = function() {
+  console.log('a');
+  if ($(this).val().length > 0 && !$(this).next().hasClass('itemTextField')) {
+  console.log('b');
+    var clone = $(this).clone();
+    clone.val('');
+    clone.insertAfter($(this));
+    clone.on('change keyup paste', cloneTextField);
+  } else if ($(this).val().length == 0 && $(this).next().hasClass('itemTextField')) {
+    $(this).remove();
+  }
+}
+
+$('.itemTextField').on('change keyup paste', cloneTextField);
