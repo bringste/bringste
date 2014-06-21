8872dd8319d5c1dd9e28861392996fdcab5ebf7b
Nico Rehwaldt <git_nikku@nixis.de>
2014-06-21 13:45:42 +0200

    chore(client): add skeleton

---
+++ front-end/lib/bringste.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var angular = require('angular');
+
+var ngModule = angular.module('bringste', [
+  require('angular-route').name,
+]);
+
+ngModule.config([ '$locationProvider', function($locationProvider) {
+  $locationProvider.html5Mode(true);
+}]);
+
+
+module.exports = ngModule;
