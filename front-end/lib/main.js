9ac9881e038a4459ced0a4fecd62d86dc3116dd6
TomKnig <hi@tomknig.de>
2014-06-21 15:43:24 +0200

    made cards expandable by click

--- front-end/lib/main.js
+++ front-end/lib/main.js
@@ -1,7 +1,5 @@
 var $ = require('jquery');
 
-alert('asd');
-
 $('.card-wrapper').click(function () {
-	$(this).toggleClass('.card-wrapper-active');
+	$(this).toggleClass('card-wrapper-active');
 });
