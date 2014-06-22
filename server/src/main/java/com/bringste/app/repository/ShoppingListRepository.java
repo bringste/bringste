4ba16ebd9a1a9550435dfea4f28bea174d206732
Andreas Drobisch <dro@unkonstant.de>
2014-06-22 07:04:12 +0200

    users lists

--- server/src/main/java/com/bringste/app/repository/ShoppingListRepository.java
+++ server/src/main/java/com/bringste/app/repository/ShoppingListRepository.java
@@ -1,7 +1,14 @@
 package com.bringste.app.repository;
 
 import com.bringste.app.domain.ShoppingList;
+import com.bringste.app.domain.User;
+import org.joda.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ShoppingListRepository extends JpaRepository<ShoppingList, String> {
+  @Query("select s from ShoppingList s where s.creator.login = ?1")
+  List<ShoppingList> findShoppingListsByLogin(String login);
 }
