434fa184fcd6692734a3b41aa2f830f75a1f0a9a
TomKnig <hi@tomknig.de>
2014-06-22 04:00:40 +0200

    fixed content insets

--- front-end/lib/lists/details/index.js
+++ front-end/lib/lists/details/index.js
@@ -16,12 +16,40 @@ var DetailsController = [ '$scope', 'focus', function($scope, focus) {
     items: [ { name: '' } ]
   };
 
+  $scope.changed = function(box) {
+
+    var items = $scope.list.items;
+
+    var idx = items.indexOf(box);
+    if (idx == items.length - 1) {
+      if (box.name) {
+        items.push({ name: '' });
+      }
+    } else {
+      if (!box.name) {
+        items.splice(idx, 1);
+
+        console.log(items[idx]);
+
+        focus(items[idx]);
+      }
+    }
+  };
+
+  $scope.deliver = function(type) {
+    $scope.list.deliver = type;
+  };
+
+  $scope.create = function() {
+    console.log($scope.list);
+  };
+
 }];
 
 
 ngModule.config([ '$routeProvider', function($routeProvider) {
 
-  $routeProvider.when('/lists/new', {
+  $routeProvider.when('/lists/details', {
     controller: DetailsController,
     template: fs.readFileSync(__dirname + '/view.html', 'utf-8')
   });
